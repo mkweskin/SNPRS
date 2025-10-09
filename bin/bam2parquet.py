2b0f2322853df7a7f8d86028113d48dbdd807f37
BobLiterman <jazz710@gmail.com>
2025-10-09 16:26:48 -0400

    Update b2pq

--- bin/bam2parquet.py
+++ bin/bam2parquet.py
@@ -4,6 +4,7 @@ import os
 import sys
 from collections import Counter
 from concurrent.futures import ProcessPoolExecutor,as_completed
+from multiprocessing import Lock
 import pyarrow.parquet as pq
 import pyarrow as pa
 from typing import List
@@ -19,33 +20,84 @@ import subprocess
 from io import StringIO
 import pysam
 import tempfile
+import math
+import csv
+
+lock = Lock()
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Process BAM/pileup file into SNPRS parquet")
     
     parser.add_argument("--bam", dest="bam_file", type=str, required=True,help="Path to input BAM file")
-    parser.add_argument("--pileup", dest="pileup_file", type=str, default=None,help="Optional: Path to input pileup file processed in SNPRS fashion")
-    parser.add_argument("--parquet", dest="parquet_file", type=str, default=None,help="Path to output Parquet file [Default: bam_file/pileup file with .parquet extension]")
-    parser.add_argument("--cpus", dest="user_cpu", type=int, default=None,help="Number of CPUs requested [Default: all available CPUs]")
-
     parser.add_argument("--fasta", dest="fasta_file", type=str, required=True,help="Path to reference FASTA file")
-
+    parser.add_argument("--parquet", dest="parquet_file", type=str, default=None,help="Path to output Parquet file [Default: bam_file/pileup file with .parquet extension]")
+    
     parser.add_argument("--mapq", dest="mapq", type=int, default=15,help="Mapping quality argument for mpileup -q [Default: 15]")
     parser.add_argument("--baseq", dest="baseq", type=int, default=15,help="Base quality argument for mpileup -Q [Default: 15]")
+    parser.add_argument("--adj_coef", dest="adj_coef", type=int, default=50,help="Adjustment coefficient for mpileup -C [Default: 50]")
     
+    parser.add_argument("--cpus", dest="user_cpu", type=int, default=None,help="Number of CPUs requested [Default: all available CPUs]")
+    parser.add_argument("--chunk_factor", dest="chunk_factor", type=int, default=1,help="Multiply CPU count by chunk_factor when calculating chunk_size (higher value, smaller chunks)")
+
     return parser.parse_args()
 
-def process_chunk(path, i, start, end):
-    
-    data_dir = os.path.dirname(path)
-    sample_name = os.path.splitext(os.path.basename(path))[0]
+def generate_pileup(bam_file,fasta_file,mapq,baseq,adj_coef,pileup_path):
 
-    chunk_file = os.path.join(data_dir,sample_name+f"_Chunk_{i}.parquet")
+    cmd = (
+        f"samtools mpileup -q {mapq} -Q {baseq} -C {adj_coef} -f {fasta_file} --ff 3844 --no-output-ends --no-output-del {bam_file} "
+        f"| awk 'NF >= 4'"
+    )
+
+    with open(pileup_path, "w") as pileup_file:
+        subprocess.run(
+            cmd,
+            shell=True,
+            check=True,
+            stdout=pileup_file,
+            text=True
+        )  
+
+def process_pileup_row(row):
+    contig, pos, ref_base, coverage, sample_bases = row.values
+
+    freqs, depth = get_base_freqs(sample_bases, ref_base)
+
+    for base, freq in freqs.items():
+        yield {
+            "contig_id": contig,
+            "contig_position": int(pos),
+            "depth": int(depth),
+            "base": base,
+            "frequency": float(freq)
+        }
+
+def get_base_freqs(sample_bases,ref_base):
+
+    s = re.sub(r"-\d+", "", sample_bases)
+    ins_counter = Counter()
+    ins_pattern = r"\+(\d+)([ACGTNacgtn]+)"
+
+    for match in re.finditer(ins_pattern, s):
+        length = int(match.group(1))
+        seq = match.group(2)[:length].upper()
+        indel_str = f"+{length}{seq}"
+        count = s.count(indel_str)
+        ins_counter[indel_str] += count
+ 
+    s = re.sub(ins_pattern, "", s).replace("*", "-").replace(".", ref_base).replace(",", ref_base).upper()
+    depth = len(s)
+
+    freqs = {b: c / depth for b, c in Counter(s).items()}
+    indel_freqs = {indel: c / depth for indel, c in ins_counter.items()}
+    freqs.update(indel_freqs)
+
+    return freqs, depth
+
+def process_chunk(pileup_file,temp_file,start,end):
 
     columns = ["Scaffold", "Position", "Ref_Base", "Coverage", "Sample_Bases", "Code"]
-
     chunk_df = pd.read_csv(
-        path,
+        pileup_file,
         sep="\t",
         header=None,
         names=columns,
@@ -60,176 +112,84 @@ def process_chunk(path, i, start, end):
             "Code": str
         }
     ).drop(columns=["Code"])
-    
-    chunk_df = chunk_df[chunk_df["Coverage"] >= 1]
 
     records = []
-
     for _, row in chunk_df.iterrows():
-        sample_bases = row["Sample_Bases"]
-        ref_base = row["Ref_Base"]
-        scaffold = row["Scaffold"]
-        position = row["Position"]
+        for record in process_pileup_row(row):
+            records.append([
+                record["contig_id"],
+                record["contig_position"],
+                record["depth"],
+                record["base"],
+                record["frequency"]
+            ])
 
-        s = re.sub(r"-\d+", "", sample_bases)
-        ins_counter = Counter()
-        ins_pattern = r"\+(\d+)"
-        
-        while True:
-            match = re.search(ins_pattern, s)
-            if not match:
-                break
-            length = int(match.group(1))
-            start_i = match.start()
-            end_i = match.end() + length
-            seq = s[match.end():end_i].upper()
-            indel_str = f"+{length}{seq}"
-            ins_counter[indel_str] += 1
-            s = s[:start_i] + s[end_i:]
-        
-        s = s.replace("*", "-").replace(".", ref_base).replace(",", ref_base).upper()
-        depth = len(s)
-        freqs = {b: c / depth for b, c in Counter(s).items()}
-        indel_freqs = {indel: c / depth for indel, c in ins_counter.items()}
-        freqs.update(indel_freqs)
+    with lock:
+        with open(temp_file, "a", newline="") as out_f:
+            writer = csv.writer(out_f, delimiter="\t")
+            writer.writerows(records)
 
-        base_rec = {
-            "contig_id": scaffold.strip().split()[0],
-            "contig_position": position,
-            "depth": depth
-        }
-        
-        for base, freq in freqs.items():
-            rec = base_rec.copy()
-            rec.update({"base": base, "frequency": freq})
-            records.append(rec)
+def compute_depth_stats(temp_file):
 
-    col_types = {
-        "contig_id": str,
-        "contig_position": int,
-        "base": str,
-        "depth": int,
-        "frequency": float
+    depth_df = (
+        pl.scan_csv(temp_file,separator="\t",has_header=True)
+        .select(["contig_id", "contig_position", "depth"])
+        .unique(subset=["contig_id", "contig_position"])
+        .collect()
+    )
+
+    covered_sites = depth_df.height
+
+    q25 = round(depth_df["depth"].quantile(0.25, "nearest"), 2)
+    q50 = round(depth_df["depth"].quantile(0.5, "nearest"), 2)
+    q75 = round(depth_df["depth"].quantile(0.75, "nearest"), 2)
+    mean_depth = round(depth_df["depth"].mean(), 2)
+    min_depth = int(depth_df["depth"].min())
+    max_depth = int(depth_df["depth"].max())
+
+    stats = {
+        "covered": str(covered_sites),
+        "min": str(min_depth),
+        "mean": str(mean_depth),
+        "max": str(max_depth),
+        "q25": str(q25),
+        "q50": str(q50),
+        "q75": str(q75)
     }
 
-    if not records:
-        return None
-    else:
-        (
-            pl.LazyFrame(records)
-            .select(["contig_id", "contig_position", "base", "depth", "frequency"])
-            .cast({
-                "contig_id": pl.Utf8,
-                "contig_position": pl.Int64,
-                "base": pl.Utf8,
-                "depth": pl.Int64,
-                "frequency": pl.Float64,
-            })
-            .sink_parquet(chunk_file, compression="snappy")
-        )
+    return stats
 
-        return chunk_file
+def compute_freq_stats(temp_file):
 
-def convert_bam(bam_file,pileup_path,fasta_file,user_cpu, mapq, baseq):
+    freq_df = (
+        pl.scan_csv(temp_file,separator="\t",has_header=True)
+        .select(['frequency'])
+        .collect()
+    )
     
-    convert_cmd = f"samtools view -@ {user_cpu} -q {mapq} -h -F 3844 {bam_file} | samtools mpileup -Q {baseq} --no-output-ends --no-output-del -f {fasta_file} - | awk 'NF >= 4 && ($4 > 0 || $4 != "")'"
+    freqs = freq_df["frequency"]
 
-    with open(pileup_path, "w") as pileup_file:
-        subprocess.run(
-            convert_cmd,
-            shell=True,
-            check=True,
-            stdout=pileup_file,
-            stderr=subprocess.DEVNULL,
-            text=True
-        )
+    total_alleles = freqs.len()
 
-def process_pileup(pileup_file,user_cpu,contig_map):
-        
-    with open(pileup_file, "r") as f:
-        line_count = sum(1 for _ in f)
-
-    if line_count == 0:
-        os.remove(pileup_file)
-        raise ValueError("Pileup file was empty.")
-
-    n_chunks = min(line_count, user_cpu)
-    chunk_size = (line_count + n_chunks - 1) // n_chunks
+    stats = {
+        "Allele_Count": str(total_alleles),
+        "BT_0_1": f"{(freqs < 0.01).sum()}",
+        "BT_1_5": f"{((freqs >= 0.01) & (freqs < 0.05)).sum()}",
+        "BT_5_10": f"{((freqs >= 0.05) & (freqs < 0.10)).sum()}",
+        "BT_10_15": f"{((freqs >= 0.10) & (freqs < 0.15)).sum()}",
+        "BT_15_85": f"{((freqs >= 0.15) & (freqs < 0.85)).sum()}",
+        "BT_85_90": f"{((freqs >= 0.85) & (freqs < 0.90)).sum()}",
+        "BT_90_95": f"{((freqs >= 0.90) & (freqs < 0.95)).sum()}",
+        "BT_95_99": f"{((freqs >= 0.95) & (freqs < 0.99)).sum()}",
+        "BT_99_100": f"{(freqs >= 0.99).sum()}",
+    }
     
-    jobs = []
-    for i in range(n_chunks):
-        start = i * chunk_size
-        end = min((i + 1) * chunk_size, line_count)
-        jobs.append((pileup_file, i, start, end))
+    return stats
+
+def get_arrow(temp_file,contig_map,metadata):
     
-    # Process chunks
-    chunk_files = []
-    with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(process_chunk, path, i, start, end) for path, i, start, end in jobs]
-        for future in as_completed(futures):
-            chunk_files.append(future.result())
-
-    return chunk_files
-                        
-def depth_stats(chunk_file):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    worker_script = os.path.join(script_dir, "helper_scripts/depth_stats.py")
-    
-    cmd = ["python", worker_script,chunk_file]
-    result = subprocess.run(cmd,
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True
-        )
-    
-    if result.returncode != 0:
-        raise RuntimeError(
-            f"Failed to process depth data.\n"
-            f"Command: {' '.join(cmd)}\n"
-            f"Return Code: {result.returncode}\n"
-            f"STDOUT:\n{result.stdout}\n"
-            f"STDERR:\n{result.stderr}"
-        )
-    try:
-        output = json.loads(result.stdout)
-    except json.JSONDecodeError:
-        raise
-
-    return output
-
-def freq_stats(chunk_file):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    worker_script = os.path.join(script_dir, "helper_scripts/freq_stats.py")
-    
-    cmd = ["python", worker_script,chunk_file]
-    result = subprocess.run(cmd,
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True
-        )
-    
-    if result.returncode != 0:
-        raise RuntimeError(
-            f"Failed to process depth data.\n"
-            f"Command: {' '.join(cmd)}\n"
-            f"Return Code: {result.returncode}\n"
-            f"STDOUT:\n{result.stdout}\n"
-            f"STDERR:\n{result.stderr}"
-        )
-    try:
-        output = json.loads(result.stdout)
-    except json.JSONDecodeError:
-        raise
-
-    return output
-
-def get_arrow(chunk_file,metadata):
-    
-    with open(chunk_file, "r") as f:
-        files = [line.strip() for line in f if line.strip()]
-
-    if not files:
-        raise ValueError("No chunk files listed in " + chunk_file)
-
-    lazy_frames = [pl.scan_parquet(f) for f in files]
     return (
-        pl.concat(lazy_frames)
+        pl.scan_csv(temp_file,separator="\t",has_header=True)
         .with_columns([
             pl.col("contig_id").replace_strict(contig_map, default=None).alias("contig_index")
         ])
@@ -247,67 +207,106 @@ def get_arrow(chunk_file,metadata):
         .replace_schema_metadata(metadata)
     )
 
-#### MAIN ####
+##### Main #####
+
+# region 00: Parse args
+
 args = parse_args()
 
 user_cpu = args.user_cpu if args.user_cpu else os.cpu_count()
-
 mapq = args.mapq
 baseq = args.baseq
+adj_coef = args.adj_coef
+chunk_factor = args.chunk_factor
 
-
-# Process reference fasta
 fasta_file = os.path.abspath(args.fasta_file)
 if not os.path.exists(fasta_file):
     sys.exit(f"{fasta_file} does not exist...")
-raw_records = [(rec.id, str(rec.seq)) for rec in SeqIO.parse(fasta_file, "fasta")]
-if not raw_records:
-    sys.exit("No contigs found.")
-raw_records = natsorted(raw_records, key=lambda x: x[0])
 
-total_sites = sum(len(seq) for _, seq in raw_records)
-contig_ids = [rec_id.strip().split()[0] for rec_id, _ in raw_records]
-contig_map = {v: k for k, v in enumerate(contig_ids)}
-
-# Process BAM file
 bam_file = os.path.abspath(args.bam_file)
 if not os.path.exists(bam_file):
     sys.exit(f"{bam_file} does not exist...")
+data_dir = os.path.dirname(bam_file)
+sample_name = os.path.splitext(os.path.basename(bam_file))[0]
+
 with pysam.AlignmentFile(bam_file, "rb") as bamfile:
     paired = any(read.is_paired for i, read in enumerate(bamfile.fetch(until_eof=True)) if i < 1000)
 
-# Check for pileup file
-if args.pileup_file:
-    pileup_file = os.path.abspath(args.pileup_file)
-    if not os.path.exists(pileup_file):
-        sys.exit(f"{pileup_file} does not exist...")  
-    data_dir = os.path.dirname(pileup_file)
-    sample_name = os.path.splitext(os.path.basename(pileup_file))[0]
-else:
-    data_dir = os.path.dirname(bam_file)
-    sample_name = os.path.splitext(os.path.basename(bam_file))[0]
-    pileup_path = os.path.join(data_dir,sample_name+".pileup")
-    pileup_file = convertBAM(bam_file,pileup_path,fasta_file,user_cpu,mapq,baseq)
-
-# Set ouptut file
 if not args.parquet_file:
-    parquet_file = os.path.join(data_dir,sample_name+".parquet")
+    parquet_file = os.path.join(data_dir,sample_name+"_Raw.parquet")
 else:
     parquet_file = os.path.abspath(args.parquet_file)
 
 if os.path.exists(parquet_file):
     sys.exit(f"{parquet_file} already exists...")    
 
-# Convert pileup to chunks
-chunk_files = process_pileup(pileup_file,user_cpu,contig_map)
-chunk_file = os.path.join(data_dir,sample_name+"_Chunks.txt")
+# endregion
 
-with open(chunk_file, mode="w+") as file:
-    file.write("\n".join(chunk_files) + "\n")
+# region 01: Process reference FASTA
 
-depth_stats = depth_stats(chunk_file)
+raw_records = [(rec.id, str(rec.seq)) for rec in SeqIO.parse(fasta_file, "fasta")]
+
+if not raw_records:
+    sys.exit("No contigs found.")
+
+raw_records = natsorted(raw_records, key=lambda x: x[0])    
+
+total_sites = sum(len(seq) for _, seq in raw_records)
+contig_ids = [rec_id.strip().split()[0] for rec_id, _ in raw_records]
+contig_map = {v: k for k, v in enumerate(contig_ids)}
+
+# endregion
+
+# region 02: Process BAM into pileup
+
+pileup_file = os.path.join(data_dir,sample_name+".pileup")
+if os.path.exists(pileup_file):
+    sys.exit(f"{pileup_file} already exists...")    
+
+generate_pileup(bam_file,fasta_file,mapq,baseq,adj_coef,pileup_file)
+
+with open(pileup_file, "r") as f:
+    line_count = sum(1 for _ in f)
+
+if line_count == 0:
+    raise ValueError("Pileup file was empty.")
+
+# endregion
+
+# region 03: Process pileup in parallel
+temp_file = os.path.join(data_dir,sample_name+"_Temp.tsv")
+
+if os.path.exists(temp_file):
+    os.remove(temp_file)
+
+with open(temp_file, "w", newline="") as out_f:
+    writer = csv.writer(out_f, delimiter="\t")
+    writer.writerow(["contig_id", "contig_position", "depth", "base", "frequency"])
+
+n_chunks = min(line_count, user_cpu*chunk_factor)
+chunk_size = (line_count + n_chunks - 1) // n_chunks
+
+jobs = []
+for i in range(n_chunks):
+    start = i * chunk_size
+    end = min((i + 1) * chunk_size, line_count)
+    jobs.append((pileup_file,temp_file, start, end))
+
+with ProcessPoolExecutor(max_workers = user_cpu) as executor:
+    futures = [executor.submit(process_chunk, pileup,temp,start, end) for pileup,temp, start, end in jobs]
+    for future in as_completed(futures):
+        try:
+            future.result()
+        except Exception as e:
+            print(f"Error in worker: {e}")
+        
+# endregion
+
+# region 04: Summarize and save 
+
+depth_stats = compute_depth_stats(temp_file)
 percent_covered = f"{int(depth_stats['covered'])/int(total_sites):.2f}"
-freq_stats = freq_stats(chunk_file)
+freq_stats = compute_freq_stats(temp_file)
 
 metadata = {
     "sample_id": sample_name,
@@ -323,20 +322,12 @@ metadata = {
 final_metadata = {k.encode(): str(v).encode() for k, v in metadata.items()}
 
 try:
-    arrow_table = get_arrow(chunk_file, final_metadata)
+    arrow_table = get_arrow(temp_file, contig_map,final_metadata)
     pq.write_table(arrow_table, parquet_file, compression="snappy")
 except Exception as e:
     raise e
 else:
-    with open(chunk_file, "r") as f:
-        files = [line.strip() for line in f if line.strip()]
-    for f in files:
-        try:
-            os.remove(f)
-        except FileNotFoundError:
-            pass
-    os.remove(chunk_file)
+    os.remove(temp_file)
+    os.remove(pileup_file)
 
-
-
-
+# endregion
