de7f876065838ba3dd1280cbccec3ab739ceca91
Robert Literman <jazz710@gmail.com>
2026-04-24 10:16:02 -0400

    Don't summarize during compileJoin

--- bin/helper_scripts/compile_join.py
+++ bin/helper_scripts/compile_join.py
@@ -114,66 +114,70 @@ try:
     pl.concat(lazy_code_frames).sink_parquet(output_code_file, compression="snappy")
     pl.concat(lazy_site_frames).sink_parquet(output_site_file, compression="snappy")
 
-    # endregion
-
-    # region 02: Summarize sample data
-
-    lazy_codes = pl.scan_parquet(output_code_file)
-    sample_ids = lazy_codes.collect_schema().names()[2:]
-
-    sample_summary_list = [summarize_sample(s, output_code_file) for s in sample_ids]
-    pl.concat(sample_summary_list, how="vertical").to_pandas().to_csv(output_sample_summary, sep="\t", index=False)
+    sample_ids = pl.scan_parquet(output_code_file).collect_schema().names()[2:]
 
     # endregion
 
-    # region 03: Summarize site data 
-
-    lazy_sites = pl.scan_parquet(output_site_file)
-
-    code_labels = {
-        0: "Other",
-        1: "Pure_Fixed",
-        2: "Pure_Biallelic",
-        3: "Pure_Triallelic",
-        4: "Pure_Quadallelic",
-        5: "Pure_Pentallelic",
-        6: "Fixed_wSingleton",
-        7: "Biallelic_wSingleton",
-        8: "Triallelic_wSingleton",
-        9: "Quadallelic_wSingleton",
-    }
-
-    df_counts = (
-        lazy_sites
-        .filter(pl.col("Site_Code") != 0)
-        .with_columns([
-            pl.col("Site_Code").replace_strict(code_labels).alias("Site_Type")
-        ])
-        .group_by(["Missing", "Site_Type"])
-        .agg(pl.len().alias("count"))
-        .collect()
-        .to_pandas()
-    )
-
-    summary_pivot = df_counts.pivot_table(
-        values="count",
-        index="Missing",
-        columns="Site_Type",
-        aggfunc="first",
-        fill_value=0
-    ).reset_index()
-
-    summary_pivot.to_csv(output_missing_summary, sep="\t", index=False)
+    ### PAUSE ###
+    
+#    # region 02: Summarize sample data
+#    lazy_codes = pl.scan_parquet(output_code_file)
+#    sample_ids = lazy_codes.collect_schema().names()[2:]
+#
+#    sample_summary_list = [summarize_sample(s, output_code_file) for s in sample_ids]
+#    pl.concat(sample_summary_list, how="vertical").to_pandas().to_csv(output_sample_summary, sep="\t", index=False)
+#
+#    # endregion
+#
+#    # region 03: Summarize site data 
+#
+#    lazy_sites = pl.scan_parquet(output_site_file)
+#
+#    code_labels = {
+#        0: "Other",
+#        1: "Pure_Fixed",
+#        2: "Pure_Biallelic",
+#        3: "Pure_Triallelic",
+#        4: "Pure_Quadallelic",
+#        5: "Pure_Pentallelic",
+#        6: "Fixed_wSingleton",
+#        7: "Biallelic_wSingleton",
+#        8: "Triallelic_wSingleton",
+#        9: "Quadallelic_wSingleton",
+#    }
+#
+#    df_counts = (
+#        lazy_sites
+#        .filter(pl.col("Site_Code") != 0)
+#        .with_columns([
+#            pl.col("Site_Code").replace_strict(code_labels).alias("Site_Type")
+#        ])
+#        .group_by(["Missing", "Site_Type"])
+#        .agg(pl.len().alias("count"))
+#        .collect()
+#        .to_pandas()
+#    )
+#
+#    summary_pivot = df_counts.pivot_table(
+#        values="count",
+#        index="Missing",
+#        columns="Site_Type",
+#        aggfunc="first",
+#        fill_value=0
+#    ).reset_index()
+#
+#    summary_pivot.to_csv(output_missing_summary, sep="\t", index=False)
 
     joined_info = {
         "Join_ID":join_id,
         "Joined_Directory":join_directory,
-        "Sample_IDs":",".join(sample_ids),
+        #"Sample_IDs":",".join(sample_ids),
         "Scaffold_File":scaffold_parquet,
         "Code_File":output_code_file,
-        "Site_File":output_site_file,
-        "Sample_Summary_File":output_sample_summary,
-        "Site_Count_File":output_missing_summary
+        "Site_File":output_site_file
+        #,
+        #"Sample_Summary_File":output_sample_summary,
+        #"Site_Count_File":output_missing_summary
         }
 
     with open(output_json_file, "w", encoding="utf-8") as f:
