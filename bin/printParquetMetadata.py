1fc70015c00da00779f75146dcd64e203aa5dad5
Robert Literman <jazz710@gmail.com>
2026-06-22 13:01:16 -0400

    Update print script

--- bin/printParquetMetadata.py
+++ bin/printParquetMetadata.py
@@ -10,14 +10,17 @@ fixed_codes = pl.Series([1, 2, 3, 4, 16, 33,34,35,36,48])
 def parse_args():
     parser = argparse.ArgumentParser(description="Print the header metadata from a parquet file")
     parser.add_argument("-p","--p", dest="parquet", type=str, required=True,help="Path to parquet file")
-    parser.add_argument("-d","--d", dest="data", action="store_true",help="Preview the top 100 rows of the data table")
-    parser.add_argument("--idx", dest="idx",type=int,default = None, help="Preview the top 100 rows of the data table")
+    parser.add_argument("-d","--d", dest="data", action="store_true",help="Preview the top 50 rows of the data table")
+    parser.add_argument("--idx", dest="idx",type=int,default = None, help="Preview the top 50 rows of the data table from contig --idx")
     parser.add_argument("--pos", dest="pos",type=int, default = None,help="Preview the top 100 rows of the data table")
     return parser.parse_args()
 
 def parquet_preview(parquet_path,preview_data):
 
     parquet_file = pq.ParquetFile(parquet_path)
+    row_count = parquet_file.metadata.num_rows
+    print(f"\nTotal rows: {row_count}")
+    
     metadata = parquet_file.schema_arrow.metadata
     
     if metadata is None or len(metadata) == 0:
@@ -32,41 +35,27 @@ def parquet_preview(parquet_path,preview_data):
 
     if preview_data:
         lf = pl.read_parquet(parquet_path).lazy()
+            
+        if args.idx is not None:
+            
+            if args.pos is not None:
+                idx_df = lf.filter((pl.col("contig_index") == args.idx) & (pl.col("contig_position") == args.pos)).collect(engine="streaming")
+            
+            else:
+                idx_df = lf.filter((pl.col("contig_index") == args.idx)).sort(['contig_index','contig_position']).collect(engine="streaming")
+                            
+            with pl.Config(tbl_cols=-1, tbl_width_chars=-1,tbl_rows=-1):
+                print(idx_df)
+        else:
+            head_df = lf.limit(25).collect()
+            tail_df = lf.tail(25).collect()
 
-        row_count = pq.ParquetFile(parquet_path).metadata.num_rows
-        print(f"\nTotal rows: {row_count}")
-        head_df = lf.limit(25).collect()
+            with pl.Config(tbl_cols=-1, tbl_width_chars=-1, tbl_rows=50):
+                print("\n=== TOP 25 ===")
+                print(head_df)
 
-
-        pl.Config.set_tbl_rows(5)
-        print(head_df)
-
-
-        """
-        dup_rows = (
-            lf.join(
-            lf.group_by(["contig_index", "contig_position"])
-            .len()
-            .filter(pl.col("len") > 1),
-            on=["contig_index", "contig_position"],
-            how="inner"
-        )
-        ).limit(25).collect()
-
-        print("\nFirst 25 dup rows:")
-        pl.Config.set_tbl_rows(25)
-        print(dup_rows)
-        """
-        
-        if args.idx is not None and args.pos is not None:
-            filtered_df = (
-                lf.filter(
-                    (pl.col("contig_index") == args.idx) & (pl.col("contig_position") == args.pos)
-                )
-                .limit(25)
-                .collect()
-            )
-            print(filtered_df)
+                print("\n=== BOTTOM 25 ===")
+                print(tail_df)
 
 
 args = parse_args()
